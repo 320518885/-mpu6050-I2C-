58df98c891e427358321b17ad1e4aeccc61efc74
Mean <320518885@qq.com>
2024-12-25 22:49:17 +0800

    1

---
+++ MDK-ARM/1234/eventrecorder.d
@@ -0,0 +1,35 @@
+1234\eventrecorder.o: D:\Alway using\Keil5\keil\ARM\PACK\Keil\ARM_Compiler\1.3.1\Source\EventRecorder.c
+1234\eventrecorder.o: .\RTE\_1234\RTE_Components.h
+1234\eventrecorder.o: ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h
+1234\eventrecorder.o: ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f429xx.h
+1234\eventrecorder.o: ../Drivers/CMSIS/Include/core_cm4.h
+1234\eventrecorder.o: D:\Alway using\Keil5\keil\ARM\ARMCC\Bin\..\include\stdint.h
+1234\eventrecorder.o: ../Drivers/CMSIS/Include/cmsis_version.h
+1234\eventrecorder.o: ../Drivers/CMSIS/Include/cmsis_compiler.h
+1234\eventrecorder.o: ../Drivers/CMSIS/Include/cmsis_armcc.h
+1234\eventrecorder.o: ../Drivers/CMSIS/Include/mpu_armv7.h
+1234\eventrecorder.o: ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h
+1234\eventrecorder.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h
+1234\eventrecorder.o: ../Core/Inc/stm32f4xx_hal_conf.h
+1234\eventrecorder.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h
+1234\eventrecorder.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h
+1234\eventrecorder.o: ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h
+1234\eventrecorder.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h
+1234\eventrecorder.o: D:\Alway using\Keil5\keil\ARM\ARMCC\Bin\..\include\stddef.h
+1234\eventrecorder.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h
+1234\eventrecorder.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h
+1234\eventrecorder.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h
+1234\eventrecorder.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h
+1234\eventrecorder.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h
+1234\eventrecorder.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h
+1234\eventrecorder.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h
+1234\eventrecorder.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h
+1234\eventrecorder.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h
+1234\eventrecorder.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h
+1234\eventrecorder.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h
+1234\eventrecorder.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h
+1234\eventrecorder.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim.h
+1234\eventrecorder.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h
+1234\eventrecorder.o: D:\Alway using\Keil5\keil\ARM\ARMCC\Bin\..\include\string.h
+1234\eventrecorder.o: D:\Alway using\Keil5\keil\ARM\PACK\Keil\ARM_Compiler\1.3.1\Include\EventRecorder.h
+1234\eventrecorder.o: .\RTE\Compiler\EventRecorderConf.h
---
+++ MDK-ARM/1234/retarget_io.d
@@ -0,0 +1,9 @@
+1234\retarget_io.o: D:\Alway using\Keil5\keil\ARM\PACK\Keil\ARM_Compiler\1.3.1\Source\retarget_io.c
+1234\retarget_io.o: D:\Alway using\Keil5\keil\ARM\ARMCC\Bin\..\include\string.h
+1234\retarget_io.o: D:\Alway using\Keil5\keil\ARM\ARMCC\Bin\..\include\stdint.h
+1234\retarget_io.o: D:\Alway using\Keil5\keil\ARM\ARMCC\Bin\..\include\stdio.h
+1234\retarget_io.o: D:\Alway using\Keil5\keil\ARM\ARMCC\Bin\..\include\stdlib.h
+1234\retarget_io.o: D:\Alway using\Keil5\keil\ARM\ARMCC\Bin\..\include\assert.h
+1234\retarget_io.o: D:\Alway using\Keil5\keil\ARM\ARMCC\Bin\..\include\rt_sys.h
+1234\retarget_io.o: .\RTE\_1234\RTE_Components.h
+1234\retarget_io.o: D:\Alway using\Keil5\keil\ARM\PACK\Keil\ARM_Compiler\1.3.1\Include\EventRecorder.h
